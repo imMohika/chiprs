3bcd2d3a4661ec03b25be44675a15d5efbb02276
imMohika <im@mohika.ir>
2025-07-29 15:54:22 +0330

    feat: implement missing opcodes

--- src/emu/mod.rs
+++ src/emu/mod.rs
@@ -1,7 +1,10 @@
 pub mod constants;
 mod fontset;
 
-use crate::emu::constants::{RAM_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH, STACK_SIZE, START_ADDR, V_SIZE};
+use rand::random;
+use crate::emu::constants::{
+    RAM_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH, STACK_SIZE, START_ADDR, V_SIZE,
+};
 use crate::emu::fontset::{FONTSET, FONTSET_SIZE};
 
 pub struct Emulator {
@@ -68,7 +71,7 @@ impl Emulator {
         }
 
         if self.sound_timer > 0 {
-            if self.sound_timer == 1{
+            if self.sound_timer == 1 {
                 // TODO)) beep beep
             }
             self.sound_timer -= 1;
@@ -85,37 +88,130 @@ impl Emulator {
     }
 
     fn execute(&mut self, op: u16) {
-       let d1 = ((op & 0xF000) >> 12) as u8;
+        let d1 = ((op & 0xF000) >> 12) as u8;
         let d2 = ((op & 0x0F00) >> 8) as u8;
         let d3 = ((op & 0x00F0) >> 4) as u8;
-        let d4 = ((op & 0x000F) >> 0) as u8;
+        let d4 = (op & 0x000F) as u8;
 
-        match (d1, d2,d3,d4) {
+        match (d1, d2, d3, d4) {
             // 0000: NOP
-            (0,0,0,0) => return,
+            (0, 0, 0, 0) => return,
             // 00E0: Clear screen
-            (0,0,0xE,0) => {
+            (0, 0, 0xE, 0) => {
                 self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
             }
             // 1NNN: jump
-            (1,_,_,_) => {
+            (1, _, _, _) => self.counter = op & 0xFFF,
+            // BNNN: jump to NNN + V0
+            (0xB, _, _, _) => self.counter = (op & 0xFFF) + self.v_reg[0] as u16,
+
+            // 00EE: return from a subroutine
+            (0, 0, 0xE, 0xE) => self.counter = self.pop(),
+            // 2NNN: execute subroutine
+            (2, _, _, _) => {
+                self.push(self.counter);
                 self.counter = op & 0xFFF
             }
+
+            // 3XNN: skip if vx == nn
+            (3, _, _, _) => {
+                let nn = (op & 0xFF) as u8;
+                if self.v_reg[d2 as usize] == nn {
+                    self.counter += 2;
+                }
+            }
+            // 4XNN: skip if vx != nn
+            (4, _, _, _) => {
+                let nn = (op & 0xFF) as u8;
+                if self.v_reg[d2 as usize] != nn {
+                    self.counter += 2;
+                }
+            }
+            // 5XY0: skip if vx == vy
+            (5, _, _, _) => {
+                if self.v_reg[d2 as usize] == self.v_reg[d3 as usize] {
+                    self.counter += 2;
+                }
+            }
+            // 9XY0: skip if vx == vy
+            (9, _, _, _) => {
+                if self.v_reg[d2 as usize] != self.v_reg[d3 as usize] {
+                    self.counter += 2;
+                }
+            }
+
             // 6XNN: set register VX
-            (6, _,_,_) => {
+            (6, _, _, _) => {
                 self.v_reg[d2 as usize] = (op & 0xFF) as u8;
             }
             // 7XNN: add value to register VX
-            (7, _,_,_) => {
+            (7, _, _, _) => {
                 let nn = (op & 0xFF) as u8;
                 self.v_reg[d2 as usize] = self.v_reg[d2 as usize].wrapping_add(nn);
             }
+
+            // 8XY0: store the value of VY in VX
+            (8, _, _, 0) => {
+                self.v_reg[d2 as usize] = self.v_reg[d3 as usize];
+            }
+            // 8XY1: Set VX to VX or VY
+            (8, _, _, 1) => {
+                self.v_reg[d2 as usize] |= self.v_reg[d3 as usize];
+            }
+            // 8XY2: Set VX to VX AND VY
+            (8, _, _, 2) => {
+                self.v_reg[d2 as usize] &= self.v_reg[d3 as usize];
+            }
+            // 8XY3: Set VX to VX XOR VY
+            (8, _, _, 3) => {
+                self.v_reg[d2 as usize] ^= self.v_reg[d3 as usize];
+            }
+            // 8XY4: Add VY to VX
+            (8, _, _, 4) => {
+                let (vx, carry) = self.v_reg[d2 as usize].overflowing_add(self.v_reg[d3 as usize]);
+                self.v_reg[d2 as usize] = vx;
+                self.v_reg[0xF] = if carry { 1 } else { 0 };
+            }
+            // 8XY5: Sub VY from VX
+            (8, _, _, 5) => {
+                let (vx, borrow) = self.v_reg[d2 as usize].overflowing_sub(self.v_reg[d3 as usize]);
+                self.v_reg[d2 as usize] = vx;
+                self.v_reg[0xF] = if borrow { 0 } else { 1 };
+            }
+            // 8XY6: right shift VX
+            (8, _, _, 6) => {
+                // starting with CHIP-48 and SUPER-CHIP in the early 1990s, these instructions were changed so that they shifted VX in place, and ignored the Y completely.
+                self.v_reg[d2 as usize] = self.v_reg[d3 as usize];
+                let least_sig = self.v_reg[d2 as usize] & 1;
+                self.v_reg[d2 as usize] >>= 1;
+                self.v_reg[0xF] = least_sig;
+            }
+            // 8XYE: left shift VX
+            (8, _, _, 0xE) => {
+                self.v_reg[d2 as usize] = self.v_reg[d3 as usize];
+                let most_sig = (self.v_reg[d2 as usize] >> 7) & 1;
+                self.v_reg[d2 as usize] <<= 1;
+                self.v_reg[0xF] = most_sig;
+            }
+
+            // 8XY7: set VX to VY - VX
+            (8, _, _, 7) => {
+                let (vx, borrow) = self.v_reg[d3 as usize].overflowing_sub(self.v_reg[d2 as usize]);
+                self.v_reg[d2 as usize] = vx;
+                self.v_reg[0xF] = if borrow { 0 } else { 1 };
+            }
+
             // ANNN: set index register I
-            (0xA, _,_,_) => {
+            (0xA, _, _, _) => {
                 self.i_reg = op & 0xFFF;
             }
+            // CXNN: VX = rand & NN
+            (0xC, _, _, _) => {
+                let rnd: u8 = random();
+                self.v_reg[d2 as usize] = rnd & (op & 0xFF) as u8;
+            }
             // DXYN: display/draw
-            (0xD,_,_,_) => {
+            (0xD, _, _, _) => {
                 let x_cord = self.v_reg[d2 as usize] as usize;
                 let y_cord = self.v_reg[d3 as usize] as usize;
                 let rows = d4;
@@ -138,8 +234,51 @@ impl Emulator {
 
                 self.v_reg[0xF] = if any_flipped { 1 } else { 0 };
             }
+            // TODO)) EX9E: skip if VX key is pressed
+            // TODO)) EXA1: skip if VX key is not pressed
+            
+            // FX07: store delay timer in VX
+            (0xF,_,0,7) => {
+                self.v_reg[d2 as usize] = self.delay_timer;
+            }
+            // TODO)) FX0A: get key
+            // FX15: delay timer = VX
+            (0xF,_,1,5) => {
+                self.delay_timer = self.v_reg[d2 as usize];
+            }
+            // FX18: sound timer = VX
+            (0xF,_,1,8) => {
+                self.sound_timer = self.v_reg[d2 as usize];
+            }
+            // FX1E: add VX to I
+            (0xF,_,1,0xE) => {
+                self.i_reg = self.i_reg.wrapping_add(self.v_reg[d2 as usize] as u16)
+            }
+            // FX29: font character
+            (0xF,_,2,9) => {
+                self.i_reg = (self.v_reg[d2 as usize] * 5) as u16
+            }
+            // FX33: binary-coded decimal conversion
+            (0xF,_,3,3) => {
+                let vx = self.v_reg[d2 as usize];
+                self.ram[self.i_reg as usize] = vx / 100;
+                self.ram[self.i_reg as usize + 1] = (vx / 10) % 10;
+                self.ram[self.i_reg as usize + 2] = vx % 10;
+            }
+            // FX55: save V0..VX into I
+            (0xF,_,5,5) => {
+                for idx in 0..=d2 as usize {
+                    self.ram[self.i_reg as usize + idx] = self.v_reg[idx]
+                }
+            }
+            // FX65: load I into V0..VX
+            (0xF,_,6,5) => {
+                for idx in 0..=d2 as usize {
+                    self.v_reg[idx] = self.ram[self.i_reg as usize + idx]
+                }
+            }
 
-            (_,_,_,_) => unimplemented!("unimplemented operation: {}", op),
+            (_, _, _, _) => unimplemented!("unimplemented operation: {:#04x}", op),
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+use crate::emu::Emulator;
+use crate::emu::constants::{SCREEN_HEIGHT, SCREEN_WIDTH};
+use minifb::{Key, Window, WindowOptions};
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use minifb::{Key, Window, WindowOptions};
-use crate::emu::constants::{SCREEN_HEIGHT, SCREEN_WIDTH};
-use crate::emu::Emulator;
 
 mod emu;
 
-const SCALE : usize = 10;
+const SCALE: usize = 10;
 const WINDOW_WIDTH: usize = SCREEN_WIDTH * SCALE;
 const WINDOW_HEIGHT: usize = SCREEN_HEIGHT * SCALE;
 
@@ -24,20 +24,20 @@ fn main() {
     let mut buffer = Vec::new();
     rom.read_to_end(&mut buffer).expect("unable to read rom");
     emu.load(&buffer);
-    
+
     let mut window = Window::new(
         "Chiprs",
         WINDOW_WIDTH,
         WINDOW_HEIGHT,
         WindowOptions::default(),
     )
-        .unwrap_or_else(|e| {
-            panic!("{}", e);
-        });
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
 
     window.set_target_fps(60);
-    window.set_background_color(0,0,0);
-    
+    window.set_background_color(0, 0, 0);
+
     while window.is_open() && !window.is_key_down(Key::Escape) {
         emu.tick();
         emu.tick_timers();
@@ -66,7 +66,7 @@ fn draw_rect(window_buffer: &mut [u32], x: usize, y: usize) {
             let window_x = start_x + x_offset;
             let window_y = start_y + y_offset;
             let idx = window_y * WINDOW_WIDTH + window_x;
-            
+
             // Set the pixel color to white
             window_buffer[idx] = 0x00FFFFFF;
         }
